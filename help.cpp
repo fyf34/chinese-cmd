27c74a2e0650a520bcf003b76578ac11007a7b85
fyf34 <136884370+fyf34@users.noreply.github.com>
2023-07-17 10:53:08 +0800

    Update help.cpp

--- help.cpp
+++ help.cpp
@@ -1,7 +1,11 @@
 #include<iostream>
 using namespace std;
 int main(){
-	cout<<"关机  关闭您的电脑"<<endl;
-	cout<<"退出  关闭chinese-cmd.exe"<<endl;
+	cout<<"Tips:以下内容按拼音首字母排序"<<endl;
+	cout<<"B"<<endl;
 	cout<<"遍历  输出该子目录下所有文件信息"<<endl;
+	cout<<"G"<<endl;
+	cout<<"关机  关闭您的电脑"<<endl;
+	cout<<"T"<<endl;
+	cout<<"退出  关闭chinese-cmd.exe"<<endl;
 }
