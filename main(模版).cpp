6babdadc4b0d0b685a48a58047c5f9f7c8f33752
fyf34 <136884370+fyf34@users.noreply.github.com>
2023-07-17 11:21:57 +0800

    Update main(模版).cpp

--- main(模版).cpp
+++ main(模版).cpp
@@ -7,16 +7,15 @@ int main(){
 	cout<<"2023 fishoj 保留所有权利。"<<endl<<endl;
 	while(1){
 		string b;
-		cout << a;
+		cout<<a;
 		cin>>b; 
-		if(b=="关机") {
-			system("shutdown -p"); 	
+		if(b=="关机"){
+			system("shutdown -p");
 		}else if(b=="退出"){
 			return 0;
 		}else if(b=="遍历"){
 			system("dir/s"); 
-		}
-		else{
+		}else{
 			cout<<"'"<<b<<"' 不是内部或外部命令，也不是可运行的程序"<<endl;
 			cout<<"或批处理文件。"<<endl<<endl;
 		}
